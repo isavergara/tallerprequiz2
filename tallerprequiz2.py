c174ce637da1f90037892daf332caad243f4c74a
isavergara <isabella.vergara@udea.edu.co>
2024-04-11 14:47:25 -0500

    2.Crea una copia de la matriz creada en el ítem anterior(usar método copy)de solo 3 dimensiones (“Cortando una de las dimensiones”)

--- tallerprequiz2.py
+++ tallerprequiz2.py
@@ -11,3 +11,12 @@ matriz = np.random.rand(10, 10, 10, 1200)
 # Print de la matriz
 print(matriz)
 
+#2Crea una copia de la matriz creada en el ítem anterior(usar método copy)de solo 3 dimensiones (“Cortando una de las dimensiones”)
+# Crear una copia de la matriz anterior con solo 3 dimensiones
+matriz_3d = matriz.copy()[:, :, :, 0]
+
+# Imprimir la matriz 3D, dimensiones y tamaño
+print("\nMatriz de 3 dimensiones:")
+print(matriz_3d)
+print("\nDimensiones:", matriz_3d.ndim)
+print("Tamaño:", matriz_3d.size)
