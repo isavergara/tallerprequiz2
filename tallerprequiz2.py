e7f3aa18d7d29f390e46bc407524942f77a772e9
isavergara <isabella.vergara@udea.edu.co>
2024-04-11 14:42:51 -0500

    1 Crear una matriz de Numpy aleatoria de 4 dimensiones y un sizede 1200000

--- tallerprequiz2.py
+++ tallerprequiz2.py
@@ -3,3 +3,11 @@ import numpy as np
 import pandas as pd
 import scipy.io as sio
 
+# 1 Crear una matriz de Numpyaleatoria de 4dimensiones y un sizede 1200000 
+
+#  matriz de 4 dimensiones
+matriz = np.random.rand(10, 10, 10, 1200)
+
+# Print de la matriz
+print(matriz)
+
